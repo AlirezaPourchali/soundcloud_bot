6ba46c92350de6218301166279b3cc7a7cc067d2
AlirezaPourchali <arp.joker82@gmail.com>
2022-11-08 22:24:44 +0330

    sed line num

--- bash.sh
+++ bash.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-echo `curl https://soundcloud.com/soroush-habibi-419430096/likes` > last.html
-
-sed -n '/<section>/,/<\/section>/p' last.html > new.html
-
+curl https://soundcloud.com/soroush-habibi-419430096/likes > curl.html
+sed -n '/<section>/,/<\/section>/p' curl.html > last.html
+i=`awk '/itemprop/{ print NR } last.html' | wc -l`
+awk '/itemprop/ { print NR } last.html' > line.txt
+for n in {1..$i};do
+	a=`sed -n '$n p' line.txt`
+	sed -n '$a p' last.html > line.html
+done
