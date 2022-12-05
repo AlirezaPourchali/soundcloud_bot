e4253fec086bf2d3e22e9c8305b8225dda3851e5
AlirezaPourchali <arp.joker82@gmail.com>
2022-12-05 21:02:24 +0330

    finally

--- bash.sh
+++ bash.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-curl https://soundcloud.com/soroush-habibi-419430096/likes > curl.html
-sed -n '/<section>/,/<\/section>/p' curl.html > last.html
-i=`awk '/itemprop/{ print NR } last.html' | wc -l`
-awk '/itemprop/ { print NR } last.html' > line.txt
-for n in {1..$i};do
-	a=`sed -n '$n p' line.txt`
-	sed -n '$a p' last.html > line.html
-done
+curl https://soundcloud.com/arpjoker-82/likes | sed -n '/<section>/,/<\/section>/p'| grep url | awk 'BEGIN{FS="href"}{print $2}' | awk 'BEGIN{FS="\""}{print "https://soundcloud.com"$2}' > curl.html
+
+#awk '/itemprop/ { print NR }' last.html > line.txt
+#i=`wc -l line.txt | cut -b 1`
+#printf -v i '%d\n' "$i" 2>/dev/null
+#o=1
+#for n in {1..$i};do
+#	a=`cut -f ` 
+#	sed -n ''$a'p' last.html > line.html
+#		echo $o
+#	o=$((o+1))
+#
+#done
